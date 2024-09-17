cafcfe7725abae515c6c4e81927e281b37a188a9
Abylai <erniyazov2005@gmail.com>
2024-09-17 15:38:05 +0500

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,12 +10,7 @@ class Main {
     public static void main(String[] args) {
         System.out.println("asqeeeeeeeeee");
 
-        System.out.println("update");
+        System.out.println("updateeeeeeeeeeeeeeeeeeeeeeeeee");
 
-        System.out.println("dasda");
-        lol(1,3);
-    }
-    static void lol(int a,int b){
-        System.out.println(a+b);
     }
 }
